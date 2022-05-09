9a4a52cbe351fe9f6cb5819d78751ff0e0111966
eth-from-zero <1050565481@qq.com>
2022-05-09 23:29:03 +0800

    callback in rust

--- src/examples/tableview_example/src/main.rs
+++ src/examples/tableview_example/src/main.rs
@@ -4,12 +4,10 @@ mod generated_code {
 }
 pub use generated_code::*;
 
+use slint::{Model, VecModel};
+
 fn main() {
     let view = TableView::new();
-
-    view.on_right_click_on_the_item(|index, x, y| {
-        println!("index={}, x={}, y={}", index, x, y);
-    });
 
     view.on_right_click_on_the_view(|index, x, y, t| {
         println!("view: index={}, x={}, y={}, text={}", index, x, y, t);
@@ -17,6 +15,22 @@ fn main() {
 
     view.on_menu_on_clicked(|index, value| {
         println!("menu index = {}, value = {}", index, value);
+    });
+
+    let v_clone = view.as_weak();
+
+    view.on_right_click_view(move |mx, my| {
+        let v = v_clone.unwrap();
+        let current_index = my / v.get_item_height();
+        let rows = v.get_rows();
+        rows.as_any().downcast_ref::<VecModel<Row>>().unwrap();
+        println!(
+            "current_index={}, mx={}, my={}, item-text={}",
+            current_index,
+            mx,
+            my,
+            rows.row_data(current_index as usize).unwrap().text
+        );
     });
 
     view.run();
