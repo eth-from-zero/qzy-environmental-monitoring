8df66abaf69df532ab52c73258d285b1cc8fa8f0
eth-from-zero <1050565481@qq.com>
2022-06-13 22:54:43 +0800

    update

--- src/config_ui/build.rs
+++ src/config_ui/build.rs
@@ -1,4 +1,10 @@
 fn main() {
     slint_build::compile("ui/main.slint").unwrap();
+    // slint_build::compile("ui/testmain.slint").unwrap();
+    // slint_build::compile_with_config(
+    //     "ui/main.slint",
+    //     slint_build::CompilerConfiguration::new()
+    //         .with_include_paths(vec!["ui/main.slint".into(), "ui/testmain.slint".into()]),
+    // )
+    // .unwrap();
 }
-
--- src/config_ui/src/main.rs
+++ src/config_ui/src/main.rs
@@ -1,5 +1,6 @@
 use config_ui::ComponentHandle;
 use config_ui::Main;
+// use config_ui::TestMain;
 
 fn main() {
     let win = Main::new();
