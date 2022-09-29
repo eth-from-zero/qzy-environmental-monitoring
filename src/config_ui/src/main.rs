37fab420d34d63508c97d4201f128018b898041c
eth-from-zero <1050565481@qq.com>
2022-09-29 23:45:37 +0800

    update

--- src/config_ui/src/main.rs
+++ src/config_ui/src/main.rs
@@ -5,5 +5,10 @@ use config_ui::Main;
 fn main() {
     let win = Main::new();
 
+    let win_weak = win.as_weak();
+    win.on_other_page_data_query_and_table_report_dialog_to_single_right_clicked(move || {
+        let win = win_weak.unwrap();
+    });
+
     win.run();
 }
