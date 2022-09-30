1f304d1a193fae0211c4458dd5f7694ca07e57c7
eth-from-zero <1050565481@qq.com>
2022-09-30 20:34:09 +0800

    update

--- src/config_ui/src/main.rs
+++ src/config_ui/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
     let win_weak = win.as_weak();
     win.on_other_page_data_query_and_table_report_dialog_to_single_right_clicked(move || {
         let win = win_weak.unwrap();
+
+        win.get_other_page_data_query_and_table_report_dialog_left_rows();
     });
 
     win.run();
