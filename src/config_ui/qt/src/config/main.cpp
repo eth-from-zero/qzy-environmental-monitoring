a54b210c4d34692d9855199a35a6709b6cbd6b02
eth-from-zero <1050565481@qq.com>
2022-10-06 22:16:48 +0800

    update

--- src/config_ui/qt/src/config/main.cpp
+++ src/config_ui/qt/src/config/main.cpp
@@ -1,11 +1,11 @@
-#include "mainwindow.h"
+#include "pages/welcome.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    Welcome w;
     w.show();
     return a.exec();
 }
