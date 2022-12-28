ee3391e1a2dad5276d3286cb7b654a8e7c839058
eth-from-zero <1050565481@qq.com>
2022-12-28 22:31:40 +0800

    update

--- src/config_ui/qt/src/config/headers/widgets/variant_edit/extend/logicexpr.h
+++ src/config_ui/qt/src/config/headers/widgets/variant_edit/extend/logicexpr.h
@@ -1,4 +1,20 @@
 #ifndef LOGICEXPR_H
 #define LOGICEXPR_H
 
+#include <QDialog>
+#include <QStandardItemModel>
+
+#include "ui_ve_ed_logicexpr.h"
+
+class Logicexpr : public QDialog {
+public:
+    Logicexpr(QWidget* parent = nullptr);
+    ~Logicexpr() = default;
+
+private:
+    std::unique_ptr<Ui::VEEDLogicexpr> ui_;
+    std::unique_ptr<QStandardItemModel> left_list_view_model_;
+    std::unique_ptr<QStandardItemModel> right_list_view_model_;
+};
+
 #endif // LOGICEXPR_H
