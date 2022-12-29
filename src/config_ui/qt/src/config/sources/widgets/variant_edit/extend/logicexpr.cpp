04919f91f164209af2cd9570e09f51b42e19695b
eth-from-zero <1050565481@qq.com>
2022-12-29 21:39:54 +0800

    update

--- src/config_ui/qt/src/config/sources/widgets/variant_edit/extend/logicexpr.cpp
+++ src/config_ui/qt/src/config/sources/widgets/variant_edit/extend/logicexpr.cpp
@@ -4,38 +4,29 @@
 
 #include "headers/widgets/variant_edit/extend/logicexpr.h"
 
-Selectunit::Selectunit(QWidget*): QDialog(nullptr) {
-    ui_ = std::make_unique<Ui::VEUSSelectunit>();
+Logicexpr::Logicexpr(QWidget*): QDialog(nullptr) {
+    ui_ = std::make_unique<Ui::VEEDLogicexpr>();
     ui_->setupUi(this);
 
     {
-        left_table_view_model_ = std::make_unique<QStandardItemModel>();
-        left_table_view_model_->setHorizontalHeaderLabels({"单位类型"});
+        left_list_view_model_ = std::make_unique<QStandardItemModel>();
+        left_list_view_model_->setHorizontalHeaderLabels({"单位类型"});
         {
             auto item = new QStandardItem();
             item->setCheckable(true);
             item->setCheckState(Qt::Unchecked);
             item->setText("气浓度");
-            left_table_view_model_->setItem(0, 0, item);
+            left_list_view_model_->appendRow(item);
         }
         {
             auto item = new QStandardItem();
             item->setCheckable(true);
             item->setCheckState(Qt::Unchecked);
             item->setText("水浓度");
-            left_table_view_model_->setItem(1, 0, item);
+            left_list_view_model_->setItem(1, 0, item);
         }
-        ui_->tv_left->setModel(left_table_view_model_.get());
 
-        ui_->tv_left->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
-    }
-
-    {
-        right_table_view_model_ = std::make_unique<QStandardItemModel>();
-        right_table_view_model_->setHorizontalHeaderLabels({"名称", "转换公式", "反转公式"});
-        ui_->tv_right->setModel(right_table_view_model_.get());
-
-        ui_->tv_right->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+        ui_->lv_expr->setModel(left_list_view_model_.get());
     }
 
     connect(ui_->btn_ok, &QPushButton::clicked, this, [this]() {
