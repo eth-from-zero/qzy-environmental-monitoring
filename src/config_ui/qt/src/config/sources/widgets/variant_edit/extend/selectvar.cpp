78c92c78becc2661da9a7b3f41074fcbcfd64200
eth-from-zero <1050565481@qq.com>
2023-01-12 22:46:44 +0800

    update

--- src/config_ui/qt/src/config/sources/widgets/variant_edit/extend/selectvar.cpp
+++ src/config_ui/qt/src/config/sources/widgets/variant_edit/extend/selectvar.cpp
@@ -33,10 +33,22 @@ Selectvar::Selectvar(QWidget*): QDialog(nullptr) {
             item->setText("kpa");
             left_table_view_model_->setItem(1, 1, item);
         }
-        ui_->tv_left->setModel(right_table_view_model_.get());
+        ui_->tv_left->setModel(left_table_view_model_.get());
         ui_->tv_left->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 
         right_table_view_model_ = std::make_unique<QStandardItemModel>();
+        {
+            auto item = new QStandardItem();
+            item->setCheckable(false);
+            item->setText("湿度");
+            right_table_view_model_->setItem(0, 0, item);
+        }
+        {
+            auto item = new QStandardItem();
+            item->setCheckable(false);
+            item->setText("%");
+            right_table_view_model_->setItem(0, 1, item);
+        }
         right_table_view_model_->setHorizontalHeaderLabels({"名称", "单位"});
         ui_->tv_right->setModel(right_table_view_model_.get());
         ui_->tv_left->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
