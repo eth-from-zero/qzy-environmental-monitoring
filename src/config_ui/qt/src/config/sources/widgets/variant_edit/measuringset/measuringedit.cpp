1a9d7477bdb1808c68d62c3c6d4ba1ec713c5070
eth-from-zero <1050565481@qq.com>
2023-01-24 22:35:16 +0800

    update

--- src/config_ui/qt/src/config/sources/widgets/variant_edit/measuringset/measuringedit.cpp
+++ src/config_ui/qt/src/config/sources/widgets/variant_edit/measuringset/measuringedit.cpp
@@ -7,4 +7,8 @@
 Measuringedit::Measuringedit(QWidget*): QDialog(nullptr) {
     ui_ = std::make_unique<Ui::VEMEMeasuringedit>();
     ui_->setupUi(this);
+
+    connect(ui_->btn_ok, &QPushButton::clicked, this, [this]() {
+        this->reject();
+    });
 }
