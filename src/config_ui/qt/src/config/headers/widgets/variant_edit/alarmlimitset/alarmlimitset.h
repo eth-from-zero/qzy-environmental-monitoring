000b26e0f392eeb94183a73dd5575d9cfc2818c2
eth-from-zero <1050565481@qq.com>
2023-02-10 22:25:56 +0800

    update

--- src/config_ui/qt/src/config/headers/widgets/variant_edit/alarmlimitset/alarmlimitset.h
+++ src/config_ui/qt/src/config/headers/widgets/variant_edit/alarmlimitset/alarmlimitset.h
@@ -1,4 +1,18 @@
 #ifndef ALARMLIMITSET_H
 #define ALARMLIMITSET_H
 
+#include <QDialog>
+#include <QStandardItemModel>
+
+#include "ui_ve_me_measuringedit.h"
+
+class AlarmlimitsetEdit : public QDialog {
+public:
+    AlarmlimitsetEdit(QWidget* parent = nullptr);
+    ~AlarmlimitsetEdit() = default;
+
+private:
+    std::unique_ptr<Ui::VEAEEdit> ui_;
+};
+
 #endif // ALARMLIMITSET_H
