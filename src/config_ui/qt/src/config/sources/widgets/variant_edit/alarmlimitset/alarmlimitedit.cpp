d4d24a8ba2d161f6c859ae1ed3f214c6d299a8d3
eth-from-zero <1050565481@qq.com>
2023-02-14 21:00:35 +0800

    update

--- src/config_ui/qt/src/config/sources/widgets/variant_edit/alarmlimitset/alarmlimitedit.cpp
+++ src/config_ui/qt/src/config/sources/widgets/variant_edit/alarmlimitset/alarmlimitedit.cpp
@@ -1,1 +1,13 @@
 #include "headers/widgets/variant_edit/alarmlimitset/alarmlimitedit.h"
+
+Measuringedit::Measuringedit(QWidget*): QDialog(nullptr) {
+    ui_ = std::make_unique<Ui::VEMEMeasuringedit>();
+    ui_->setupUi(this);
+
+    connect(ui_->btn_ok, &QPushButton::clicked, this, [this]() {
+        this->reject();
+    });
+    connect(ui_->btn_cancel, &QPushButton::clicked, this, [this]() {
+        this->reject();
+    });
+}
