41c04aaffa50d0b5354c8fdbf55e33ad7f15fd18
eth-from-zero <1050565481@qq.com>
2023-03-02 20:28:48 +0800

    update

--- src/config_ui/qt/src/config/headers/widgets/switching.h
+++ src/config_ui/qt/src/config/headers/widgets/switching.h
@@ -1,4 +1,33 @@
 #ifndef SWITCHINT_H
 #define SWITCHING_H
 
+#include <memory>
+#include "ui_switching.h"
+
+#include <QWidget>
+#include <QMenu>
+#include <QStandardItemModel>
+
+#include "headers/widgets/variant_edit/variant_edit.h"
+
+//QT_BEGIN_NAMESPACE
+//namespace Ui { class Welcome; }
+//QT_END_NAMESPACE
+
+class MeasuringPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MeasuringPage(QWidget *parent= nullptr);
+    ~MeasuringPage() = default;
+
+private:
+    std::unique_ptr<QMenu> list_view_menu_;
+    std::unique_ptr<Ui::Measuring> ui_;
+    std::unique_ptr<QStandardItemModel> list_view_model_;
+    std::unique_ptr<VariantEdit> variant_edit_;
+};
+
+
 #endif // SWITCHING_H
