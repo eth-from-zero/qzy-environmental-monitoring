0b6d74975b1525403e524009d5cabb5442439ba8
eth-from-zero <1050565481@qq.com>
2023-04-14 23:35:48 +0800

    udpate

--- src/config_ui/qt/src/config/sources/widgets/other/operator_config/operator_config.cpp
+++ src/config_ui/qt/src/config/sources/widgets/other/operator_config/operator_config.cpp
@@ -12,6 +12,14 @@ OperatorConfig::OperatorConfig(QWidget *parent)
    atomic_widget_ = std::make_unique<OOCAtomic>(ui_->tab_atomic);
    process_widget_ = std::make_unique<OtherOCProcess>(ui_->tab_process);
    sysmode_widget_ = std::make_unique<OtherOCSysmode>(ui_->tab_sysmode);
+
+   connect(ui_->btn_add, &QPushButton::clicked, this, [this]() {
+       auto tw_index = ui_->tabWidget->currentIndex();
+       if (tw_index == 1) {
+       } else if (tw_index == 2) {
+       } else {
+       }
+   });
 }
 
 }
