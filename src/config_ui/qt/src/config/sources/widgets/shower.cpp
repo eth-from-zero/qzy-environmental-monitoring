b287093d79914fdcf3df5e494f421cbc8bf51297
eth-from-zero <1050565481@qq.com>
2023-05-03 15:02:42 +0800

    update

--- src/config_ui/qt/src/config/sources/widgets/shower.cpp
+++ src/config_ui/qt/src/config/sources/widgets/shower.cpp
@@ -7,8 +7,9 @@ Shower::Shower(QWidget* parent)
     ui_ = std::make_unique<Ui::Shower>();
     ui_->setupUi(this);
 
-    QImage image(":/common/images/resources/config_main.jpeg");
+    QImage image(":/common/images/resources/shower.jpeg");
     auto pix_map = QPixmap::fromImage(image);
-//    pix_map = pix_map.scaled(200, 600, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    pix_map = pix_map.scaled(ui_->lb_image->width(), ui_->lb_image->height(), Qt::KeepAspectRatio);
+//    pix_map = pix_map.scaled(1000, 800, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
     ui_->lb_image->setPixmap(pix_map);
 }
